1f6e359dbc9528be052e9a1afa76c8a98c004848
jojoj <jojoj@JOE-LAPTOP>
2024-12-04 11:31:19 +0000

    Add project files.

---
+++ WordleGame/WordleGame.py
@@ -0,0 +1,5 @@
+
+# This is a game where a random 5-letter word is picked from a list of them, and then you have to guess what it is,
+# with correct letters being marked down, misplaced letters being shown as such and incorrect letters being shown as wrong.
+
+
